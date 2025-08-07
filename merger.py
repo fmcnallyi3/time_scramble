9222ed252995b201cd43761880eb6a5fbde8f076
Frank McNally <mcnally_ft@mercer.edu>
2025-08-07 13:21:56 -0500

    Removed dependencies on os module, other general cleanup

--- merger.py
+++ merger.py
@@ -6,8 +6,7 @@
 
 import numpy as np
 import healpy as hp
-import sys, os, argparse, re
-from glob import glob
+import argparse
 import datetime as dt
 
 from pathlib import Path
@@ -17,12 +16,12 @@ import default_paths
 
 
 # Extract map parameters other than date and detector configuration
-def map_params(filename, rm_date=True):
+def map_params(filename, ignore_last=False):
 
     map_info = Path(filename).stem          # filename ignoring path and ext
     params = map_info.split('_')
     params = params[1:]                     # ignore config (always first)
-    if rm_date:
+    if ignore_last:
         params = params[:-1]                # ignore date (always last)
 
     return '_'.join(params)
@@ -56,22 +55,6 @@ def merger(files, out, overwrite, nmaps=3):
     hp.write_map(out, combined_map)
 
 
-def projectMerge(it, prefix, omit=[], nmaps=3):
-
-    # Get fileList for given detector
-    merged_filepath = Path(prefix) / 'merged'
-    fileList = sorted(Path(prefix).glob('merged/-*.fits' % (prefix, it)))
-    # Omit detector configurations (optional)
-    for icyr in omit:
-        fileList = [f for f in fileList if icyr not in f]
-
-    # Merge maps with given parameters
-    paramList = sorted(set([map_params(f, rm_date=False) for f in fileList]))
-    for params in paramList:
-        files = [f for f in fileList if map_params(f, rm_date=False)==params]
-        out = '%s/merged/%s_%s.fits' % (prefix, it, params)
-        merger(files, out, True)
-
 
 if __name__ == "__main__":
 
@@ -85,9 +68,9 @@ if __name__ == "__main__":
     p.add_argument('--startDate', dest='startDate',
             default='05-13',
             help='Month and day at which to start the calendar year')
-    p.add_argument('--sixyear', dest='sixyear',
-            default=False, action='store_true',
-            help='Recreate maps for 6-year paper')
+    p.add_argument('--detector', dest='detector',
+            default='IC86', choices=['IC86','IT81'],
+            help='Choose to merge in-ice (IC86) or IceTop (IT81) files')
     p.add_argument('--detectorYear', dest='detectorYear',
             default=False, action='store_true',
             help='Run using detector years instead of calendar years')
@@ -101,25 +84,24 @@ if __name__ == "__main__":
 
 
     # Collect all daily map files
-    masterList = sorted(glob('%s/*/*_????-??-??.fits' % args.prefix))
+    map_path = Path(args.prefix)
+    masterList = sorted(map_path.glob(f'*/{args.detector}*_????-??-??.fits'))
 
-    # Default behavior: merge all IC86 detector configurations
-    if args.configs == None and not args.sixyear:
-        masterList = [f for f in masterList if 'IC86' in f]
-        configs = [re.findall('IC86-\d{4}',f)[0] for f in masterList]
-        args.configs = sorted(set(configs))
+    # Default behavior: merge all detector configurations
+    if args.configs == None:
+        args.configs = sorted(set([f.stem.split('_')[0] for f in masterList]))
 
     for cfg in args.configs:
 
         fileList = []
 
-        if args.sixyear or args.detectorYear:
+        if args.detectorYear:
             fileList += [f for f in masterList if cfg in f]
 
         else:
             yyyy = int(cfg[-4:])
-            s = '%i-%s' % (yyyy, args.startDate)
-            e = '%s-%s' % (yyyy+1, args.startDate)
+            s = f'{yyyy}-{args.startDate}'
+            e = f'{yyyy+1}-{args.startDate}'
             start = dt.datetime.strptime(s, '%Y-%m-%d')
             end = dt.datetime.strptime(e, '%Y-%m-%d')
             delta = end - start
@@ -131,23 +113,23 @@ if __name__ == "__main__":
         fileList.sort()
 
         # Calculate comprehensive list of all unique parameters
-        paramList = sorted(set([map_params(f) for f in fileList]))
+        paramList = sorted(set([map_params(f, True) for f in fileList]))
 
         # Run merger on each set of parameters
         for params in paramList:
-            files = [f for f in fileList if map_params(f)==params]
-            out = '%s/merged/%s_%s.fits' % (args.prefix, cfg, params)
+            files = [f for f in fileList if map_params(f, True)==params]
+            out = f'{args.prefix}/merged/{cfg}_{params}.fits'
             if args.detectorYear:
-                out = '%s/detector_merge/%s_%s.fits' % (args.prefix,cfg,params)
+                out = f'{args.prefix}/detector_merge/{cfg}_{params}.fits'
             merger(files, out, args.overwrite)
 
-    # Run project merger
-    detectorList = ['IC86']
-    omit = []
-    if args.sixyear:
-        detectorList = ['IC']
-        omit = ['IC86_'] + ['IC86-%i' % i for i in range(2015,2020)]
-    for detector in detectorList:
-        projectMerge(detector, args.prefix, omit=omit)
+
+    # Always run project merger (merge all years or detector configs)
+    files = sorted(Path(args.prefix).glob(f'merged/{detector}-*.fits'))
+    paramList = sorted(set([map_params(f) for f in files]))
+    for params in paramList:
+        files = [f for f in files if map_params(f)==params]
+        out = f'{args.prefix}/merged/{detector}_{params}.fits'
+        merger(files, out, True)
 
 
